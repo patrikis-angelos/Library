d01433c51814bd570a2c320cf8b0626f92893269
helman101 <helmanandresortegon@gmail.com>
2021-03-24 08:50:13 +0100

    create New Book button, Show Form function and .hidden CSS class

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,9 @@ let title = document.querySelector('#title')
 let author = document.querySelector('#author')
 let pages = document.querySelector('#pages')
 let button = document.querySelector('#btn')
-let read = document.querySelector('#read').checked
+let read = document.querySelector('#read')
+let form = document.querySelector('form')
+let newBookBtn = document.querySelector('#form')
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -13,12 +15,12 @@ function Book(title, author, pages, read) {
   this.read = read;
   this.info = function() {
     let alreadyRead = (read)? 'already read' : 'not read yet'
-    return title + ' by ' + author + ' ' + pages + ' pages'
+    return title + ' by ' + author + ', ' + pages + ' pages' + ', ' + alreadyRead 
   }
 };
 
 function addBookToLibrary() {
-  let newBook = new Book(title.value, author.value, pages.value, read)
+  let newBook = new Book(title.value, author.value, pages.value, read.checked)
 
   library.push(newBook)
 
@@ -28,13 +30,19 @@ function addBookToLibrary() {
 function showBook() {
   container.innerHTML = ''
   for (let i = 0; i < library.length; i++) {
-    let content = document.createElement('div')
-    let text = document.createElement('p')
-    text.textContent = library[i].info()
-    content.appendChild(text)
-    container.appendChild(content)
+    let content = document.createElement('div');
+    let text = document.createElement('p');
+    text.textContent = library[i].info();
+    content.appendChild(text);
+    container.appendChild(content);
   }
 }
 
-button.addEventListener('click', addBookToLibrary)
+function showForm() {
+  form.classList.toggle('hidden');
+  btn.classList.toggle('hidden');
+}
 
+button.addEventListener('click', addBookToLibrary);
+
+newBookBtn.addEventListener('click', showForm);
