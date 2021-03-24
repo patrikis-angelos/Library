f35729abb1f7ceb597516d3c62f81ba05e9ef71f
helman101 <helmanandresortegon@gmail.com>
2021-03-24 10:32:26 +0100

    fix linter errors

--- assets/script.js
+++ assets/script.js
@@ -1,49 +1,59 @@
 let library = [];
-let container = document.querySelector('.container')
-let title = document.querySelector('#title')
-let author = document.querySelector('#author')
-let pages = document.querySelector('#pages')
-let button = document.querySelector('#btn')
-let read = document.querySelector('#read')
-let form = document.querySelector('form')
-let newBookBtn = document.querySelector('#form')
+const container = document.querySelector('.container');
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const pages = document.querySelector('#pages');
+const button = document.querySelector('#btn');
+const read = document.querySelector('#read');
+const form = document.querySelector('form');
+const newBookBtn = document.querySelector('#form');
 
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-};
-
-Book.prototype.info = function() {
-  let alreadyRead = (this.read)? 'already read' : 'not read yet'
-  return this.title + ' by ' + this.author + ', ' + this.pages + ' pages' + ', ' + alreadyRead 
 }
 
-function addBookToLibrary() {
-  let newBook = new Book(title.value, author.value, pages.value, read.checked)
+Book.prototype.info = function () {
+  const alreadyRead = (this.read) ? 'already read' : 'not read yet';
+  return `${this.title} by ${this.author}, ${this.pages} pages, ${alreadyRead}`;
+};
 
-  library.push(newBook)
+function saveLibrary() {
+  localStorage.lib = JSON.stringify(library);
+}
 
-  saveLibrary()
-  showBooks()
+function removeBook() {
+  const { id } = this.parentNode;
+  library.splice(id, 1);
+  saveLibrary();
+  showBooks(); // eslint-disable-line
+}
+
+function changeRead() {
+  const { id } = this.parentNode;
+  const para = this.parentNode.querySelector('p');
+  library[id].read = !library[id].read;
+  para.innerHTML = library[id].info();
+  saveLibrary();
 }
 
 function showBooks() {
-  container.innerHTML = ''
-  for (let i = 0; i < library.length; i++) {
-    let content = document.createElement('div');
-    content.setAttribute("id", i);
-    let text = document.createElement('p');
+  container.innerHTML = '';
+  for (let i = 0; i < library.length; i += 1) {
+    const content = document.createElement('div');
+    content.setAttribute('id', i);
+    const text = document.createElement('p');
     text.textContent = library[i].info();
 
-    let removeBtn = document.createElement('button');
+    const removeBtn = document.createElement('button');
     removeBtn.addEventListener('click', removeBook);
     removeBtn.textContent = 'Remove';
 
-    let changeReadBtn = document.createElement('button');
+    const changeReadBtn = document.createElement('button');
     changeReadBtn.addEventListener('click', changeRead);
-    changeReadBtn.textContent = 'Readed?'
+    changeReadBtn.textContent = 'Readed?';
 
     content.appendChild(text);
     content.appendChild(changeReadBtn);
@@ -52,40 +62,30 @@ function showBooks() {
   }
 }
 
+function addBookToLibrary() {
+  const newBook = new Book(title.value, author.value, pages.value, read.checked);
+
+  library.push(newBook);
+
+  saveLibrary();
+  showBooks();
+}
+
 function showForm() {
   form.classList.toggle('hidden');
-  btn.classList.toggle('hidden');
-}
-
-function removeBook() {
-  id = this.parentNode.id;
-  library.splice(id, 1);
-  saveLibrary()
-  showBooks()
-}
-
-function changeRead() {
-  id = this.parentNode.id;
-  let para = this.parentNode.querySelector('p');
-  library[id].read = !library[id].read;
-  para.innerHTML = library[id].info();
-  saveLibrary()
-}
-
-function saveLibrary() {
-  localStorage.lib = JSON.stringify(library);
+  button.classList.toggle('hidden');
 }
 
 function loadLibrary() {
-  let books = JSON.parse(localStorage.lib)
-  for (let i = 0; i < books.length; i++) {
+  const books = JSON.parse(localStorage.lib);
+  for (let i = 0; i < books.length; i += 1) {
     Object.setPrototypeOf(books[i], Book.prototype);
   }
   return books;
 }
 
 if (localStorage.lib) {
-  library = loadLibrary()
+  library = loadLibrary();
   showBooks();
 }
 
